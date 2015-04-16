7506e004249e578708db6244a92173f629c0ada2
prideone <stephane.bolu@gmail.com>
2015-04-16 10:26:52 +0200

    add files and show, list methods and links

--- shops_manager/app/controllers/shops_controller.rb
+++ shops_manager/app/controllers/shops_controller.rb
@@ -1,8 +1,24 @@
 class ShopsController < ApplicationController
+	def index
+    	@shop = Shop.all
+  	end
+
+	def show
+    	@shop = Shop.find(params[:id])
+  	end
+
 	def new
 	end
 
 	def create
-		render plain: params[:shop].inspect
+	  @shop = Shop.new(shop_params)
+	 
+	  @shop.save
+	  redirect_to @shop
 	end
+ 
+	private
+	  def shop_params
+	    params.require(:shop).permit(:chain, :name, :latittude, :longitude, :address, :city, :zip, :phone, :coutry_code)
+	  end
 end
